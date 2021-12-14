e4c31461a4a57b35293b09152b0128fade6aeb2f
Lara <laraellenh@gmail.com>
2021-12-14 17:55:53 -0500

    commit tree 915e80288c5e80de4e9b6cba383e9c6d8353edaf

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// Code your solution in this fconst returnFirstTwoDrivers = (array) => {
+//     const returnFirstTwoDrivers = (array) => {
+//         return array.slice(0,2);
+// };
+
+// const returnLastTwoDrivers = (array) => {
+
+//     return array.splice(-2);
+// };
+
+function returnFirstTwoDrivers(array){
+    return array.slice(0,2);
+}
+
+    function returnLastTwoDrivers(array){
+        return array.slice(-2);
+    }
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareMultiplier) {
+    return function(value) {
+        return fareMultiplier * value;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (array, selectionType) =>
+{return selectionType(array);}
+
